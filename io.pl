d765d1327125299a4be7fce4af2c07ba49d86de9
biromiro <up201906272@up.pt>
2022-01-08 00:49:29 +0000

    Change appearance to unicode

--- io.pl
+++ io.pl
@@ -1,5 +1,16 @@
-side_chars(first, ['/', 'T', '\\']).
-side_chars(last,  ['\\', '1', '/']).
+encoding(utf8).
+
+:- multifile
+    user:message_property/2.
+
+user:message_property(warning, color(fg(blue))).
+
+test :-
+ print_message(warning,'here').
+
+
+side_chars(first, ['\x250F\', '\x2533\', '\x2513\']).
+side_chars(last,  ['\x2517\', '\x253B\', '\x251B\']).
 
 display_box_edges(0, _, _).
 display_box_edges(N, Mid, Edge) :- N > 0,
@@ -9,24 +20,23 @@ display_box_edges(N, Mid, Edge) :- N > 0,
                                    display_box_edges(NewN, Mid, Edge).
 
 display_intermediate_line(Size, LeftEdge, Mid, RightEdge) :- write(LeftEdge),
-                                                             write('m'),
+                                                             write('\x2501\\x2501\\x2501\'),
                                                              NewSize is Size-1,
-                                                             display_box_edges(NewSize, Mid, 'm'),
+                                                             display_box_edges(NewSize, Mid, '\x2501\\x2501\\x2501\'),
                                                              write(RightEdge).
 
 display_edge_line(Size, Side) :- side_chars(Side, [LeftCorner, Mid, RightCorner]),
                                  display_intermediate_line(Size, LeftCorner, Mid, RightCorner), nl.
 
 display_checker_line([]).
-display_checker_line([Checker | T]) :- write(o),
-                                       write('|'),
+display_checker_line([Checker | T]) :- write(' \x25CF\ '),
+                                       write('\x2503\'),
                                        display_checker_line(T).
 
 display_lines([]).
-display_lines([GSLine | GSTail]) :- write('|'),
-                                    display_checker_line(GSLine), nl,
-                                    length(GSLine, Size),
-                                    display_intermediate_line(Size, '|', '+', '|'), nl,
+display_lines([GSLine | GSTail]) :- write('\x2503\'), display_checker_line(GSLine), nl, GSTail = [], !.
+display_lines([GSLine | GSTail]) :- length(GSLine, Size),
+                                    display_intermediate_line(Size, '\x2503\', '\x2503\', '\x2503\'), nl,
                                     display_lines(GSTail).
 
 display_game(GameState) :- [GSH | _] = GameState,
